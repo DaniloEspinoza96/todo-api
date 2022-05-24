44970402050edfcc0bab093801b6ca15fb6d9c8f
DaniloEspinoza1996 <despinozap@ripley.com>
2022-05-24 18:11:29 -0400

    Add mongodb initialization

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -1,10 +1,26 @@
 const router = require('express').Router();
 
 // aquí van todas las rutas de todo
-router.get('/:name', (req, res) => {
-  res.json({
-    message: `Hola ${req.params.name}`
-  })
+// router.get('/:name', (req, res) => {
+//   res.json({
+//     message: `Hola ${req.params.name}`
+//   })
+// })
+
+// insertar un TODO
+router.post('/addtodo', (req, res) => {
+  // responder con el id del todo que se ha creado
 })
+
+// obtener todos los TODO
+
+// modificar un TODO
+
+// eliminar un TODO
+
+// conseguir TODOS por fecha y orden que se pueda elegir ASC o DESC
+
+// eliminar todos los TODO
+
 
 module.exports = router;
